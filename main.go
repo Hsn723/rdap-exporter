64e8be64058ef90b61ff4239dfbb463ee1a81fa7
Hsn723 <me@atelierhsn.com>
2026-06-10 00:59:23 +0000

    explicitly embed roots

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/hsn723/rdap-exporter/cmd"
+import (
+	"github.com/hsn723/rdap-exporter/cmd"
+	_ "golang.org/x/crypto/x509roots/fallback"
+)
 
 func main() {
 	cmd.Execute()
